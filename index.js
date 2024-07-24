646574425b4537baf5c8e0d692bc546c196ffabb
OvielFelipeMendozaPineda <127790713+OvielFelipeMendozaPineda@users.noreply.github.com>
2024-07-24 18:50:39 -0500

    feat: :sparkles: SignUpForm: Create component.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import './components/Footer/Footer.js'
 import './components/HeroSection/HeroSection.js'
 import './components/Cards/Cards.js'
 import './components/Enrollment/Enrollment.js'
+import './components/SignUpForm/PlayerSignUpForm.js'
 
 const root = document.querySelector("#root")
 const getStartedBtn = document.querySelector('#get-started')
